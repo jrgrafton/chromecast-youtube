d95fdad104cecb758447b300e45ffd5ca75c5c43
jrgrafton <jrgrafton@gmail.com>
2014-08-08 16:16:39 +0100

    Update timer fixes

--- sender/js/main.js
+++ sender/js/main.js
@@ -1,5 +1,13 @@
 window.addEventListener('load',function() {
+	var DEBUG = false;
 
+	// Turn off logging when not in debugging
+	if(!DEBUG) {
+		console.log = function() {}
+		console.debug = function() {}
+		console.info = function() {}
+	}
+	
 	// Wait for Chromecast to be detected
     window['__onGCastApiAvailable'] = function(loaded, errorInfo) {
 	   	if (loaded) {
